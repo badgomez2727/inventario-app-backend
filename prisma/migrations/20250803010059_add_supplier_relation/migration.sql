9bce43b3840981ece8291832899b06a5f6adb440
Bad Gómez <gomezposadadario@gmail.com>
2025-08-10 15:15:19 -0500

    feat: Initial PostgreSQL DB setup with Docker for local dev

--- prisma/migrations/20250803010059_add_supplier_relation/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `productos` ADD COLUMN `supplier_id` INTEGER NULL;
-
--- CreateIndex
-CREATE INDEX `productos_supplier_id_idx` ON `productos`(`supplier_id`);
-
--- AddForeignKey
-ALTER TABLE `productos` ADD CONSTRAINT `productos_supplier_id_fkey` FOREIGN KEY (`supplier_id`) REFERENCES `Supplier`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
